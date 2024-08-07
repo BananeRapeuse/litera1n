fdaa1a210d22d84252f5803c83a70de7c27b80b9
Ph0qu3_111 <79283690+BananeRapeuse@users.noreply.github.com>
2024-08-08 00:19:46 +0200

    Update checkmake.py

--- checkmake.py
+++ checkmake.py
@@ -1,5 +1,4 @@
 import os
 os.system("nuitka3 --windows-onefile checkra1n.py.py")
 os.system("nuitka3 --windows-onefile checkterm.py")
-os.system("nuitka3 --windows-onefile webra1n.py")
 os.system("nuitka3 --windows-onefile checkgui.py")
