12ed1512265117a6dfb6f19a977a272efc0d376c
Ph0qu3_111 <79283690+BananeRapeuse@users.noreply.github.com>
2024-08-07 23:34:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import os
 #import mydearpygui
 import time
 while True:
-	print("Welcome to checkra1n.py")
-	print("by Sakurai07")
-	print("#########################")
+	print("###########################")
+	print("#   Welcome to litera1n ! #")
+	print("#      by Ph0qu3_111      #")
+	print("###########################")
 	print("[0]. Install dependencies (MUST DO once)")
-	print("[1]. Jailbreak")
+	print("[1]. Terminal Jailbreak")
 	print("[2]. GUI Jailbreak")
-	print("[3]. Github")
-	print("[4]. Guide")
+	print("[3]. Github link")
+	print("[4]. Jailbreak guide")
 	print("[5]. exit")
 	print("type help for help")
 	choice = input("$ ")
@@ -19,7 +20,7 @@ while True:
 	#os.system("dir")
 	#os.system("python ipwndfu")
 	elif choice == "3":
-		print("https://github.com/sakurai07/checkra1n.py")
+		print("https://github.com/BananeRapseuse/litera1n")
 	elif choice == "4":
 		print("Go to the github repository or check README.md")
 	elif choice == "0":
