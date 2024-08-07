185ad844ca4b31de5415a6bdac11fe2abce7527b
Ph0qu3_111 <79283690+BananeRapeuse@users.noreply.github.com>
2024-08-07 23:50:24 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -53,7 +53,7 @@ class Clock(Frame):
         Window.label.configure(text=now)
         Window.after(1000, Window.update_clock)
 
-load = PIL.Image.open("checkra1n.png")
+load = PIL.Image.open("litera1n.png")
 render = PIL.ImageTk.PhotoImage(load)
 img = Label(window, image=render)
 img.image = render
