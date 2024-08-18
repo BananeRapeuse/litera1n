bf62cc41974d265426a08d2d85988a69a5c46335
Ph0qu3_111 <79283690+BananeRapeuse@users.noreply.github.com>
2024-08-18 22:35:49 +0200

    Update cpbin.bat

--- cpbin.bat
+++ cpbin.bat
@@ -1,4 +1,4 @@
 cd c:\windows\system32\
 git clone https://github.com/BananeRapeuse/litera1n
 copy c:\windows\system32\litera1n\checkra1n.exe c:\windows\system32
-copy c:\windows\system32\litera1n\checkra1n.py.exe c:\windows\system32
+copy c:\windows\system32\litera1n\checkra1npy.exe c:\windows\system32
